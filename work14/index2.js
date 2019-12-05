19149f639f3da9cffa8baecb01bc82351dd73d42
qq1836758876 <1836758876@qq.com>
2019-12-05 22:36:56 +0800

    work14

--- work14/index2.js
+++ work14/index2.js
@@ -1,22 +1,19 @@
-let endseconds = new Date().getTime() + 15*1000;
-let d = h = m = s = 0;
-let id = setInterval(seckill,1000);
-function seckill(){
-    let nowtime = new Date();
-    let remaining = parseInt((endseconds - nowtime.getTime()) / 1000);
-    if (remaining > 0) {
-        d= parseInt(remaining / 86400);
-        h= parseInt((remaining / 3600) % 24);
-        m= parseInt((remaining / 60) % 60);
-        s= parseInt(remaining % 60);
-        d =  d < 10 ? '0' + d : d; 
-        h =  h < 10 ? '0' + h : h;
-        m =  m < 10 ? '0' + m : m;
-        s =  s < 10 ? '0' + s : s;
-    }else{
-        clearInterval(id);
-        d = h = m = s = '00';
-    }
+let endseconds = new Date()
+let  d = h = m = s = 0;
+d = 0;
+h = 0;
+m = 59 - endseconds.getMinutes();
+s = 59 - endseconds.getSeconds();
+let id = setInterval(seckill, 1000);
+function seckill() {
+        s--;
+    if(s == -1){
+        s = 59;
+        m--;
+    } 
+    if(m == -1){
+        m = 59;
+    } 
     document.getElementById('d').innerHTML = d +'天';
     document.getElementById('h').innerHTML = h +'时';
     document.getElementById('m').innerHTML = m +'分';
